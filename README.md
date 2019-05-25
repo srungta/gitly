0250234eaa5e3a86991dc047228adb68a89b6e4a
Shaswat Rungta <shrung@microsoft.com>
2019-05-26 02:17:51 +0530

    Add environment config Add executasble file and lib file.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Installation
+Install Miniconda.
+Use the environment.yml file to createa an environment.
---
+++ libgitly.py
@@ -0,0 +1,12 @@
+# Imports
+import argparse
+import collections
+import configparser
+import hashlib
+import os
+import re
+import sys
+import zlib
+
+# CLI read arguments
+argparser = argparse.ArgumentParser(description="The stupid content tracker")
