7c7c9e2d443de5a4a8c2e9432e81711dbd2ac892
Shaswat Rungta <shrung@microsoft.com>
2019-05-26 14:59:52 +0530

    Added repo and indentation fixes

--- libgitly.py
+++ libgitly.py
@@ -16,17 +16,61 @@ argsubparsers.required = True
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
-    if args.command == "add": cmd_add(args)
-    elif args.command == "cat-file": cmd_cat_file(args)
-    elif args.command == "checkout": cmd_checkout(args)
-    elif args.command == "commit": cmd_commit(args)
-    elif args.command == "hash-object": cmd_hash_object(args)
-    elif args.command == "init": cmd_init(args)
-    elif args.command == "log": cmd_log(args)
-    elif args.command == "ls-tree": cmd_ls-tree(args)
-    elif args.command == "merge": cmd_merge(args)
-    elif args.command == "rebase": cmd_rebase(args)
-    elif args.command == "rev-parse": cmd_rev_parse(args)
-    elif args.command == "rm": cmd_rm(args)
-    elif args.command == "show-ref": cmd_show_ref(args)
-    elif args.command == "tag": cmd_tag(args)
+    if args.command == "add":
+        cmd_add(args)
+    elif args.command == "cat-file":
+        cmd_cat_file(args)
+    elif args.command == "checkout":
+        cmd_checkout(args)
+    elif args.command == "commit":
+        cmd_commit(args)
+    elif args.command == "hash-object":
+        cmd_hash_object(args)
+    elif args.command == "init":
+        cmd_init(args)
+    elif args.command == "log":
+        cmd_log(args)
+    elif args.command == "ls-tree":
+        cmd_ls-tree(args)
+    elif args.command == "merge":
+        cmd_merge(args)
+    elif args.command == "rebase":
+        cmd_rebase(args)
+    elif args.command == "rev-parse":
+        cmd_rev_parse(args)
+    elif args.command == "rm":
+        cmd_rm(args)
+    elif args.command == "show-ref":
+        cmd_show_ref(args)
+    elif args.command == "tag":
+        cmd_tag(args)
+
+# GitRepository
+
+
+class GitRepository(object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a git repository %s" % path)
+
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vars != 0:
+                raise Exception(
+                    "Unsupported reposirotyformatversion %s" % vers)
